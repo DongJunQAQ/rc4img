e6cf378efab381ef49b0c83638057f7107fbf51e
DongJun <you@example.com>
2025-11-07 21:46:25 +0800

    docs: 添加子命令定义代码的注释

--- cmd/decrypt.go
+++ cmd/decrypt.go
@@ -13,7 +13,7 @@ var decryptCmd = &cobra.Command{ //定义解密子命令
 	Use:   "decrypt [input-file-path]",
 	Short: "使用RC4算法解密文件",
 	Long:  `将通过encrypt子命令加密的文件解密，恢复为原始可查看的图片文件（需使用加密时的相同密钥）`,
-	Args:  cobra.ExactArgs(1), // 必须传入1个位置参数：输入要解密的文件路径
+	Args:  cobra.ExactArgs(1), // 此子命令后面必须传入1个位置参数：输入要解密的文件路径
 	Run: func(_ *cobra.Command, args []string) { // 解密逻辑执行函数，args的值为位置参数即需要解密的文件路径
 		internal.ProcessCrypt(args, "解密", outputFlag, keyFlag)
 	},
--- cmd/encrypt.go
+++ cmd/encrypt.go
@@ -13,7 +13,7 @@ var encryptCmd = &cobra.Command{ //定义加密子命令
 	Use:   "encrypt [input-file-path]",
 	Short: "使用RC4算法加密文件",
 	Long:  `将指定图片文件通过RC4算法加密，输出为不可直接查看的文件（需用decrypt子命令解密）`,
-	Args:  cobra.ExactArgs(1), // 必须传入1个位置参数：输入要加密的文件路径
+	Args:  cobra.ExactArgs(1), // 此子命令后面必须传入1个位置参数：输入要加密的文件路径
 	Run: func(_ *cobra.Command, args []string) { // 加密逻辑执行函数，args的值为位置参数即需要加密的文件路径
 		internal.ProcessCrypt(args, "加密", outputFlag, keyFlag)
 	},
